7c8a94274a45d544ad42d4a65816f2d61600cadd
DSunShine371 <pestic077@gmail.com>
2025-01-16 14:01:37 +0300

    Fix Even game

--- App/src/main/java/hexlet/code/games/Even.java
+++ App/src/main/java/hexlet/code/games/Even.java
@@ -8,8 +8,7 @@ public final class Even extends Engine {
         int randomNumber = random.nextInt(20) + 1;
         System.out.println("Question: " + randomNumber);
         boolean isEven = randomNumber % 2 == 0;
-        String correctAnswer = isEven ? "yes" : "no";
-        return correctAnswer;
+        return isEven ? "yes" : "no";
     }
 
     @Override
