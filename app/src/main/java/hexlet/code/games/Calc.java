91f4f8c2852ffb41202131f44a85d26df387e662
DSunShine371 <pestic077@gmail.com>
2025-01-17 13:00:47 +0300

    Fix Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,12 +5,12 @@ import hexlet.code.Engine;
 public final class Calc extends Engine {
     private final int num1 = getRandom(20);
     private final int num2 = getRandom(20);
-    private int correctAnswer = 500;
+    private int correctAnswer;
 
     @Override
     public String askQuestionAndReturnAnswer() {
         System.out.print("Question: ");
-        switch (getRandom(3)) {
+        switch (getRandom(2)) {
             case 0:
                 correctAnswer = num1 + num2;
                 System.out.println(num1 + " + " + num2);
