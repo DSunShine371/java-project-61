04eb2b98a698147e656c97936d14c77bacb33f9b
DSunShine371 <pestic077@gmail.com>
2025-01-17 11:27:43 +0300

    1

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    application
+    checkstyle
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
