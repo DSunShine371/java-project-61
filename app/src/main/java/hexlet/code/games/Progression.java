6e41bd8773202bfea9f00ee87a500469c9e139e0
DSunShine371 <pestic077@gmail.com>
2025-01-17 13:22:15 +0300

    Fix Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -3,16 +3,17 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public final class Progression extends Engine {
-    private int startNum = getRandom(10);
-    private final int arrayLength = 5 + getRandom(5);
-    private final int progressionStep = getRandom(15);
     @Override
     public String askQuestionAndReturnAnswer() {
+        int startNum = getRandom(10);
+        int arrayLength = 5 + getRandom(5);
+        int progressionStep = getRandom(15);
+
         String[] arrayNumbers = new String[arrayLength];
         arrayNumbers[0] = String.valueOf(startNum);
         for (int i = 1; i < arrayNumbers.length; i++) {
-            arrayNumbers[i] = String.valueOf(startNum + progressionStep);
             startNum += progressionStep;
+            arrayNumbers[i] = String.valueOf(startNum);
         }
         int hiddenCell = getRandom(arrayNumbers.length);
         String correctAnswer = arrayNumbers[hiddenCell];
