e94812c95eb6e7a9b1c01a394c1c6691de11d092
DSunShine371 <pestic077@gmail.com>
2025-01-17 11:55:32 +0300

    Fix Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -9,31 +9,10 @@ public final class Progression extends Engine {
         String[] arrayNumbers = new String[5 + random.nextInt(6)];
         arrayNumbers[0] = String.valueOf(startNum);
         int progressionStep;
-        switch (random.nextInt(3)) {
-            case 0:
-                progressionStep = random.nextInt(5) + 1;
-                for (int i = 1; i < arrayNumbers.length; i++) {
-                    arrayNumbers[i] = String.valueOf(startNum + progressionStep);
-                    startNum += progressionStep;
-                }
-                break;
-            case 1:
-                progressionStep = random.nextInt(5) + 1;
-                for (int i = 1; i < arrayNumbers.length; i++) {
-                    arrayNumbers[i] = String.valueOf(startNum + progressionStep);
-                    startNum += progressionStep;
-                    progressionStep++;
-                }
-                break;
-            case 2:
-                progressionStep = random.nextInt(4) + 2;
-                for (int i = 1; i < arrayNumbers.length; i++) {
-                    arrayNumbers[i] = String.valueOf(startNum * progressionStep);
-                    startNum *= progressionStep;
-                }
-                break;
-            default:
-                System.out.println("    Error\nSomething went wrong ;)");
+        progressionStep = random.nextInt(15) + 1;
+        for (int i = 1; i < arrayNumbers.length; i++) {
+            arrayNumbers[i] = String.valueOf(startNum + progressionStep);
+            startNum += progressionStep;
         }
         int hiddenCell = random.nextInt(arrayNumbers.length);
         String correctAnswer = arrayNumbers[hiddenCell];
